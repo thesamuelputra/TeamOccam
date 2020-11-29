9f2998913c0475377242deb597645163cfe980a2
Yoksan Herlie <yoksanh@gmail.com>
2020-11-29 12:17:40 +0000

    fix decision tree learning and choose attribute for rss

--- destree/DecisionTreeLearning.m
+++ destree/DecisionTreeLearning.m
@@ -14,42 +14,44 @@
 function tree = DecisionTreeLearning(features, label, headers, max_depth)
 % fprintf('Calculating tree depth...');
 % disp(max_depth);
-[best_attribute, best_threshold] = ChooseAttribute(features);
-tree.attribute = best_attribute;
-tree.threshold = best_threshold;
-tree.op = headers{tree.attribute};
-features1 = [];
-features2 = [];
-label1 = [];
-label2 = [];
-for i = 1:size(features(:,tree.attribute),1)
-    if (features(i,tree.attribute) < tree.threshold)
-        features1 = [features1; features(i,:)];
-        label1 = [label1; label(i)];
-    elseif (features(i, tree.attribute) >= tree.threshold)
-        features2 = [features2; features(i,:)];
-        label2 = [label2; label(i)];
-    end
-end
-% tree.features1 = features1;
-% tree.features2 = features2;
-% tree.label1 = label1;
-% tree.label2 = label2;
+min_value = 7;
+tree.op = '';
 tree.kids = [];
-tree.prediction = NaN;
-tree = Branching(tree, 1, features, features1, label1, headers, max_depth);
-tree = Branching(tree, 2, features, features2, label2, headers, max_depth);
+tree.prediction = length(label);
+
+if max_depth ~= 0 && ~isempty(features)
+    if (size(features,1) > min_value)
+        % may split node into 2 branch
+        [best_attribute, best_threshold] = ChooseAttribute(features);
+        tree.attribute = best_attribute;
+        tree.threshold = best_threshold;
+        tree.op = headers{tree.attribute};
+        features1 = [];
+        features2 = [];
+        label1 = [];
+        label2 = [];
+        for i = 1:size(features(:,tree.attribute),1)
+            if (features(i,tree.attribute) < tree.threshold)
+                features1 = [features1; features(i,:)];
+                label1 = [label1; label(i)];
+            elseif (features(i, tree.attribute) >= tree.threshold)
+                features2 = [features2; features(i,:)];
+                label2 = [label2; label(i)];
+            end
+        end
+        if (~isequal(features1, features) && ~isequal(features2, features))
+            features1(:,tree.attribute) = [];
+            features2(:,tree.attribute) = [];
+            tree.kids{1} = DecisionTreeLearning(features1, label1, headers, max_depth-1);
+            tree.kids{2} = DecisionTreeLearning(features2, label2, headers, max_depth-1);
+        else
+            % leaf node because data are not splitted
+            tree.prediction = mean(label);
+        end
+    else
+        % leaf node
+        tree.prediction = mean(label);
+    end
 end
 
-function tree = Branching(tree, kid_index, root_features, features, label, headers, max_depth)
-min_value = 20;
-if max_depth ~= 0
-    if (~isempty(features))
-        if (size(features,1) > min_value || ~isequal(root_features, features))
-            tree.kids{kid_index} = DecisionTreeLearning(features, label, headers, max_depth-1);
-        else
-            tree.prediction = mean(features);
-        end
-    end
-end
 end
