994927a0824dedf21493178c71343046bfc6d96b
David Honasan <david_honasan@live.com>
2020-12-01 23:39:08 +0000

    add p+r

--- destree/c_decisiontree.m
+++ destree/c_decisiontree.m
@@ -43,9 +43,15 @@ array_y = table2array(y);
 n = round(size(array_x,1)*0.80);
 array_x_train = array_x(1:n,:);
 array_y_train = array_y(1:n);
+array_x_test = array_x(n:length(array_x),:);
+array_y_test = array_y(n:length(array_y));
 
-tree = DecisionTreeLearning(array_x_train, array_y_train, x.Properties.VariableNames, 10);
-DrawDecisionTree(tree);
+headers = x.Properties.VariableNames;
+max_depth = 10;
+
+tree = DecisionTreeLearning(array_x_train, array_y_train, headers, max_depth);
+k_tree = TreeToKFold(array_x_train, array_y_train, headers, max_depth);
+% DrawDecisionTree(tree);
 
 function tree = DecisionTreeLearning(features, label, headers, max_depth)
 % fprintf('Calculating tree depth...');
@@ -89,9 +95,6 @@ else
 end
 
 end
-
-% something is wrong with the function, attributes return the same col over
-% and over again.
 
 function [best_attribute, best_threshold, attributes] = ChooseAttribute(features, label)
 % measures how “good” each attribute (i.e. feature) in the set is.
@@ -138,6 +141,29 @@ for i = 1:(length(col)-1)
 end
 end
 
-function hai = getMajorityValue(label)
-    hai = sum(label(:)==1) > sum(label(:)==-1);
+function majority = getMajorityValue(label)
+    majority = sum(label(:)==1) > sum(label(:)==-1);
+end
+
+function [k_tree, cv_result] = TreeToKFold(features_train, label_train, headers, max_depth)
+k = 10;
+cv_result = zeros(1,k);
+k_tree = cell(1,k);
+rowsize = size(features_train,1);
+rand = randperm(rowsize);
+shuffled_features = features_train(rand,:);
+shuffled_label = label_train(rand,:);
+start_index = 1;
+for i=1:k
+    end_index = round((rowsize/k)*i);
+    ftest = shuffled_features(start_index:end_index,:);
+    ltest = shuffled_label(start_index:end_index);
+    ftrain = features_train;
+    ltrain = label_train;
+    ftrain(start_index:end_index,:) = [];
+    ltrain(start_index:end_index) = [];
+    k_tree{i} = DecisionTreeLearning(ftrain, ltrain, headers, max_depth);
+    cv_result(1,i) = PredictTreeClassification(k_tree{i}, ftest, ltest);
+    start_index = end_index;
+end
 end
