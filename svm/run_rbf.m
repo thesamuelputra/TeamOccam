8d996f63d40f6f1f8471cbd50bacee53248d4c55
stefanliemawan <stefanliemawan@gmail.com>
2020-12-18 00:51:36 +0000

    label to labels

--- svm/run_rbf.m
+++ svm/run_rbf.m
@@ -13,7 +13,7 @@ gamma = [1, 0.1, 0.01, 0.001, 0.0001];
 epsilon = [3,3,3,3,3];
 
 norm_features_c = normalize(features_c);
-norm_labels_c = normalize(label_c);
+norm_labels_c = normalize(labels_c);
 rbf_hp_tune_c = GridSearchCV(k, norm_features_c, norm_labels_c, @rbf_c, c, gamma);
 
 norm_features_r = normalize(features_r);
