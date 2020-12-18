bc15b6cf170fb0232b7a54fac1e6f581573846e9
David Honasan <david_honasan@live.com>
2020-12-18 03:42:44 +0000

    add pics

--- svm/initialize.m
+++ svm/initialize.m
@@ -17,44 +17,46 @@ optsy.VariableTypes = "double";
 
 % Import the data
 features_c = readtable("../datasets/CTG.xls", optsx);
-labels_c = readtable("../datasets/CTG.xls", optsy);
+label_c = readtable("../datasets/CTG.xls", optsy);
 
 features_r = table2array(readtable('../datasets/ccu.csv', 'Range', 'G:CY'));
-features_r(2006,:) = [];
-labels_r = table2array(readtable('../datasets/ccu.csv', 'Range', 'DZ:DZ'));
-labels_r(2006,:) = [];
+label_r = table2array(readtable('../datasets/ccu.csv', 'Range', 'DZ:DZ'));
 
 
 %% Convert multi-class label_cs to binary (N = 1, SP = -1)
-for i = 1:height(labels_c)
-    if  labels_c.NSP(i) == 2 || labels_c.NSP(i) == 3
-        labels_c.NSP(i) = -1;
+for i = 1:height(label_c)
+    if  label_c.NSP(i) == 2 || label_c.NSP(i) == 3
+        label_c.NSP(i) = -1;
     end
 end
 
 %% Clear temporary variables
 clear optsx optsy i;
 features_c = table2array(features_c);
-labels_c = table2array(labels_c);
+label_c = table2array(label_c);
 
 %% SVM
-model1 = poly_c(normalize(features_c), normalize(labels_c), 1);
-model2 = poly_c(normalize(features_c), normalize(labels_c), 2);
-model4 = poly_c(normalize(features_c), normalize(labels_c), 3);
-model5 = poly_c(normalize(features_c), normalize(labels_c), 4);
-model3 = rbf_c(normalize(features_c), normalize(labels_c), 0.1, 1);
+%model1 = poly_c(normalize(features_c), normalize(label_c), 1);
+model2 = poly_c(normalize(features_c), label_c, 1, 2);
+%model4 = poly_c(normalize(features_c), normalize(label_c), 3);
+%model5 = poly_c(normalize(features_c), normalize(label_c), 4);
+%model3 = rbf_c(normalize(features_c), normalize(label_c), 0.1, 1);
 
-for i = 2:20
-    j = 1;
+for i = 1:20
+    j = 10;
+    if i == j
+        continue;
+    end
     sv = model2.SupportVectors;
     fig = figure('Name', 'Model'+string(i));
-    gscatter(normalize(features_c(:,j)),normalize(features_c(:,i)),normalize(label_c));
+    gscatter(normalize(features_c(:,j)),normalize(features_c(:,i)), label_c);
     hold on
     plot(sv(:,j),sv(:,i),'ko','MarkerSize',10)
-    legend(string(j),string(i),'Support Vector')
+    legend(string(model2.ClassNames(1,1)), string(model2.ClassNames(2,1)),'Support Vector')
+    xlabel(string(j));
+    ylabel(string(i));
     hold off
-    saveas(fig, fullfile('C:\Users\david\Downloads\TeamOccam\svm\figures\' +string(j), 'fig'+string(j)+'-' + string(i)), 'png');
+    saveas(fig, fullfile('C:/Users/david/Downloads/TeamOccam/svm/figures/' +string(j), 'fig'+string(j)+'-' + string(i) + '.png'));
 end
-
-% mdlr_linear = fitrsvm(features_r, labels_r, 'KernelFunction', 'linear');
-% mdlc_linear = fitcsvm(features_c, labels_c, 'KernelFunction','linear', 'BoxConstraint', 1);
+% mdlr_linear = fitrsvm(features_r, label_r, 'KernelFunction', 'linear');
+% mdlc_linear = fitcsvm(features_c, label_c, 'KernelFunction','linear', 'BoxConstraint', 1);
